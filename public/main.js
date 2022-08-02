58bb0aa3116949883105f1bd8c30831605215334
hramenko <5307967-hramenko@users.noreply.gitlab.com>
2022-08-02 04:43:48 +0000

    Update public/main.js

--- public/main.js
+++ public/main.js
@@ -22,7 +22,8 @@ const searchCompanyOrPerson = function (searchWord, containerId){
 
   //fetch('./dataorg.json')
   //fetch(CORSProxy + encodeURIComponent(APIRoot + '&by=name&obj=org&active=true&query=') + encodeURIComponent(searchWord), {mode: 'no-cors'} )
-  fetch(CORSProxy + encodeURIComponent(APIRoot + '&by=name&obj=org&active=true&query=') + encodeURIComponent(searchWord) )
+  //fetch(CORSProxy + encodeURIComponent(APIRoot + '&by=name&obj=org&active=true&query=') + encodeURIComponent(searchWord) )
+  fetch(CORSProxy + encodeURIComponent( APIRoot + '&by=name&obj=org&active=true&query=' + searchWord, {mode: 'no-cors'} ) )
     .then(response => response.json())
     .then(function (result) {
       orgDataset = [];    
