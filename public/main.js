e61f4bdab866c7d7e52f034763376e3b1d9d0830
hramenko <5307967-hramenko@users.noreply.gitlab.com>
2022-08-03 15:36:00 +0000

    Replace main.js

--- public/main.js
+++ public/main.js
@@ -4,49 +4,65 @@ const APIReqCompany = 'https://api.checko.ru/v2/company?key=' + APIKey;
 const APIReqPerson = 'https://api.checko.ru/v2/entrepreneur?key=' + APIKey;
 const CORSProxy = 'https://corsproxy.io/?';
 //https://checko.ru/search/quick_tips?query=%D0%A0%D0%9E%D0%A1%D0%A2%D0%95%D0%9B%D0%95%D0%9A%D0%9E%D0%9C
+fontkit.init;
 
 var orgDataset = []
-var selectedOrgData = []
+var orgDatasetExtData = []
+//var selectedOrgData = []
 var selectedOrgExtData = []
 const container = document.getElementById('resultList');
+
 const searchform = document.getElementById('searchform');
 
 function dosearch(e) {
   e.preventDefault();
   var searchword = document.getElementById('searchword').value; 
-  searchCompanyOrPerson(searchword);
+  var isip = document.getElementById('isip').checked; 
+  searchCompanyOrPerson(searchword, isip);
 }
 
 searchform.addEventListener('submit', dosearch);
+
 
 function orgclick(e){
   let el = e.target;
   //alert(el.innerHTML);
   //alert(orgDataset[el.dataset.index]);
-  fillPdfForm(el.dataset.index, orgDataset, orgDataset, e.target.dataset.formfile )
+  fillPdfForm(el.dataset.index, e.target.dataset.formfile, el.dataset.isip )
 }
 
-const getMoreInfo = function(inn, ogrn, isIp){
- //https://corsproxy.io/?https%3A%2F%2Fapi.checko.ru%2Fv2%2Fsearch%3Fkey%3DAiBEDldGmvLAzdp3%26by%3Dname%26obj%3Dorg%26query%3D%25D1%2580%25D0%25BE%25D1%2581%25D1%2582%25D0%25B5%25D0%25BB%25D0%25B5%25D0%25BA%25D0%25BE%25D0%25BC
- //https://corsproxy.io/?https%3A%2F%2Fapi.checko.ru%2Fv2%2Fsearch%3Fkey%3DsYoECmKTCXlTF19D%26by%3Dname%26obj%3Dorg%26active%3Dtrue%26query%3D%D1%80%D0%BE%D1%81%D1%82%D0%B5%D0%BB%D0%B5%D0%BA%D0%BE%D0%BC
+function getMoreInfo (id, inn, ogrn, isIp){
+  var API = isIp ? APIReqPerson : APIReqCompany;
+  org = orgDataset[id];
+  selectedOrgExtData = [];
+  return fetch(CORSProxy + encodeURIComponent(API + '&active=true&inn='+inn+'&ogrn='+ogrn))
+  .then(response => response.json())
+  .then(function(result){
+    if (result.meta.status != 'ok') {
+      alert(result.meta.message);
+      return false;
+    }    
+    console.log(result.data);
+    //selectedOrgExtData = result.data;
+    //console.log(selectedOrgExtData);
+  }).catch(function (err){
+    alert(err.message);
+  });  
+
 }
 
-function addData(data){
-  //console.log(data)
-  
 
-  //orgDataset = orgDataset.concat(orgDataorgDatasetset, data)
-}
-
-function fetchQuery(query, pages, page) {
+function fetchQuery(query) {
   var isLastPage = false
-  var page = 1
-  //fetch(query)
-  return fetch('./data-org.json')
+  var page = 1  
+  
+  console.log(query)
+  return fetch(query)  
+  //return fetch('./data-org.json')  
   .then(response => response.json())
   .then(function (result) {    
     if (result.meta.status != 'ok') {
-      container.innerHTML = 'ошибка при получении даных';
+      container.innerHTML = result.meta.message;
       return false;
     }
     //console.log(result)
@@ -72,10 +88,37 @@ function fetchQuery(query, pages, page) {
 
     //for (let i = 0; i < result.data.Записи.length; i++){      orgDataset.push(result.data.Записи[i])    }    
     //result.data.Записи[i]
-    for(var i in result.data.Записи) {
-      orgDataset.push(result.data.Записи[i]);
-      //console.log(typeof data[i])
-      //console.log(orgDataset.length)
+  pagecount = result.data.СтрВсего;
+  //console.log('Страниц: ' + pagecount);
+
+  for(var i in result.data.Записи) {
+    if (result.data.Записи[i].ЮрАдрес != undefined){
+      var addr = result.data.Записи[i].ЮрАдрес.split(',', 2);
+      result.data.Записи[i].Индекс = addr[0].trim();
+      result.data.Записи[i].АдресБезИндекса = result.data.Записи[i].ЮрАдрес.replace(addr[0] + ',', '').trim();        
+    } else {result.data.Записи[i].ЮрАдрес = ''};
+    
+    if (result.data.Записи[i].НаимСокр == undefined) {
+      result.data.Записи[i].НаимСокр = result.data.Записи[i].ФИО;
+      result.data.Записи[i].НаимПолн = result.data.Записи[i].ФИО;
+    }
+
+    if (result.data.Записи[i].ОГРНИП != undefined) {
+      result.data.Записи[i].ОГРН = result.data.Записи[i].ОГРНИП;
+    }
+
+    if (result.data.Записи[i].ИНН == undefined) {
+      result.data.Записи[i].ИНН = '';
+    }
+
+    if (result.data.Записи[i].КПП == undefined) {
+      result.data.Записи[i].КПП = '';
+    }    
+    orgDataset.push(result.data.Записи[i]);
+  }
+  
+  if (pagecount > 1) {
+    fetchQuery(query + encodeURIComponent('&page=2'));
   }
     
 //  }  
@@ -92,29 +135,16 @@ function fetchQuery(query, pages, page) {
   });  
 }
 
-const searchCompanyOrPerson = function (searchWord){
- 
-  container.innerHTML = '';
-  //container.addEventListener("click", rowclick);
-  
+const searchCompanyOrPerson = function (searchWord, isip){
+  container.innerHTML = '';  
   orgDataset = []; 
-  //orgDataset.push('123');
-  //orgDataset.push('123');
-  //orgDataset.push('123');
-  //for (let i = 0; i < 11; i++){    orgDataset.push('asdg' + i);  }    
-
-  //const a = [];
-  //const obj = {  foo: 'bar'  }
-  //a.push(obj);
-  //console.log(a)
+  console.log(isip);
+  var searchbase = isip ? "obj=ent" : "obj=org";
   
-  fetchQuery(CORSProxy + encodeURIComponent(APIRoot + '&by=name&obj=org&active=true&source=true&query=' + encodeURIComponent(searchWord)), 1, 1)
-  .then( function(value){
+  fetchQuery(CORSProxy + encodeURIComponent(APIRoot + '&by=name&'+searchbase+'&active=true&source=true&query=' + encodeURIComponent(searchWord)))
+  .then( function(res){
     console.log(orgDataset)
-    //console.log(orgDataset instanceof Array)
-  //console.log(typeof(orgDataset[0]))
-    console.log(orgDataset.length)
-  
+    console.log(orgDataset.length)  
 
   if (orgDataset.length > 0){
     for (let i = 0; i < orgDataset.length; i++){
@@ -129,20 +159,17 @@ const searchCompanyOrPerson = function (searchWord){
       </div>
       <div class="buttons">
       </div>`;
-      li.dataset.inn = org.ИНН;
-      li.dataset.kpp = org.КПП;
-      li.dataset.ogrn = org.ОГРН;
-      li.dataset.index = i;
-     // li.addEventListener('click', rowclick);
 
       let btnC22 = document.createElement('button');
       btnC22.dataset.index = i;
+      btnC22.dataset.isip = isip;
       btnC22.innerHTML = 'C22';
       btnC22.dataset.formfile = 'form_C_22_10e.pdf';
       btnC22.addEventListener('click', orgclick);
       
       let btnF22 = document.createElement('button');
       btnF22.dataset.index = i;
+      btnC22.dataset.isip = isip;
       btnF22.innerHTML = 'Ф22';
       btnF22.dataset.formfile = 'form_F_22_10e.pdf';
       btnF22.addEventListener('click', orgclick);
@@ -150,110 +177,26 @@ const searchCompanyOrPerson = function (searchWord){
       container.appendChild(li);
       li.lastChild.appendChild(btnC22);
       li.lastChild.appendChild(btnF22);
-
-    }    
-  }
-
-});
-
-//  .then( (data) => {
-//    console.log(data)
-//   })
-  
-  //if (data != false) {    console.log(data)  }
-
-  return false
-  //fetch('./dataorg.json')
-  //fetch(APIRoot + '&by=name&obj=org&active=true&source=true&query=' + encodeURIComponent(searchWord), {mode: 'no-cors'})  
-  fetch(CORSProxy + encodeURIComponent(APIRoot + '&by=name&obj=org&active=true&source=true&query=' + encodeURIComponent(searchWord)))  
-    .then(response => response.json())
-    .then(function (result) {
-      orgDataset = [];    
-      if (result.data.status != 'error') {
-
-        for (let i = 0; i < result.data.Записи.length; i++){
-        var org = result.data.Записи[i];
-        let li = document.createElement('LI');
-        li.innerHTML = `<span>${org.НаимСокр}</span>
-        <span class="org-addr"><strong>Юр. Адрес: </strong>${org.ЮрАдрес}</span>
-        <div>
-        <span class="org-inn"><strong>ИНН: </strong>${org.ИНН}</span>
-        <span class="org-kpp"><strong>КПП: </strong>${org.КПП}</span>
-        <span class="org-ogrn"><strong>ОГРН: </strong>${org.ОГРН}</span>
-        </div>
-        <div class="buttons">
-        </div>`;
-        li.dataset.inn = org.ИНН;
-        li.dataset.kpp = org.КПП;
-        li.dataset.ogrn = org.ОГРН;
-        li.dataset.index = i;
-       // li.addEventListener('click', rowclick);
-
-        let btnC22 = document.createElement('button');
-        btnC22.dataset.index = i;
-        btnC22.innerHTML = 'C22';
-        btnC22.addEventListener('click', orgclick);
-//        let btnF22 = document.createElement('button');
-//        btnF22.dataset.index = i;
-//        btnF22.innerHTML = 'Ф22';
-//        btnF22.addEventListener('click', orgclick);
-
-        container.appendChild(li);
-        li.lastChild.appendChild(btnC22);
-//        li.lastChild.appendChild(btnF22);
-        orgDataset.push(result.data.Записи[i]);
-      }
-
+      }    
     }
-      
-      console.log(result.data.Записи[0].НаимСокр);
-      console.log(result.data.СтрВсего);
-      console.log(orgDataset);
-      let pageCount = result.data.СтрВсего;
-      if (pageCount > 1) {
-        
-      }
-
-    })
-    .catch(function (err){
-      container.innerHTML = err.message;
-    });
-}
-
-
-function getMoreData(ogrn, inn, kpp){
+  });
 
 }
+
+
 
 function fillFormField(form, fieldName, fieldText, font){
   const {PDFDocument, PDFForm, StandardFonts, PDFFont, setFontAndSize} = PDFLib
   let field = form.getTextField(fieldName);
-  if (field) {
-    //field.setFontSize(4);    
-    field.setText(fieldText);
-    //field.setFontSize(4);
-    //console.log(field.acroField.getDefaultAppearance() );
-    //const da = field.acroField.getDefaultAppearance() ?? '';
-    //const newDa = da + '\n' + setFontAndSize('Arial', 9).toString(); 
-    //const newDa = da + '\n' + setFontAndSize('HelveticaNeueCyr', 9).toString(); 
-    //const newDa = da + '\n' + setFontAndSize('Ubuntu', 9).toString(); 
-    //field.acroField.setDefaultAppearance(newDa);
-
-//    const da = field.acroField.getDefaultAppearance() ?? '';
-//    const newDa = da + '\n' + setFontAndSize('Arial', 9).toString(); 
-//    field.acroField.setDefaultAppearance(newDa);
-    
-//field.defaultUpdateAppearances(font)
-
-    field.updateAppearances(font);
-    //field.updateAppearances(font);
+  if (field != 'undefined') {
+     field.setText(fieldText);
+     field.updateAppearances(font);
   }
 }
 
-async function fillPdfForm(orgIndex, orgData, orgMoreData, formFile){
+async function fillPdfForm(orgIndex, formFile, isip){
   const {PDFDocument, PDFForm, StandardFonts, PDFFont, setFontAndSize} = PDFLib
   const formUrl = './' + formFile
-   //form_C_22_10e.pdf'
   const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
   const pdfDoc = await PDFDocument.load(formPdfBytes)  
 //  var fontkit = window.fontkit;
@@ -268,22 +211,39 @@ async function fillPdfForm(orgIndex, orgData, orgMoreData, formFile){
 
   const form = await pdfDoc.getForm()
   //await form.embedDefaultFont(customFont);
-  console.log(customFont);
+  //console.log(customFont);
   
 
   const pages = await pdfDoc.getPages();
   //const form = await pdfDoc.getForm()
   
-console.log(form)
-console.log(form.getFields());
+//console.log(form)
+//console.log(form.getFields());
 
 
   //const nameField = form.getTextField('Text10')
 //  if (nameField.isReadOnly()) console.log('Read only is enabled')
 //  if (nameField.needsAppearancesUpdate()) console.log('Needs update')
   //const innField = form.getTextField('Text11')
+  getMoreInfo(orgIndex, orgDataset[orgIndex].ИНН, orgDataset[orgIndex].ОГРН, isip)
+  .then(function(result){
+  console.log(selectedOrgExtData );
+  
+  if (orgDataset[orgIndex].ИНН == '') {selectedOrgExtData.ИНН;}
+  if (orgDataset[orgIndex].НаимСокр == '') {orgDataset[orgIndex].НаимСокр = selectedOrgExtData.ТипСокр + ' ' + selectedOrgExtData.ФИО;}
+  if (orgDataset[orgIndex].НаимПолн == '') {orgDataset[orgIndex].НаимПолн = selectedOrgExtData.ТипСокр + ' ' + selectedOrgExtData.ФИО;}
+  if (orgDataset[orgIndex].ЮрАдрес == '') {
+      orgDataset[orgIndex].ЮрАдрес = selectedOrgExtData.СвязРуковод[0].ЮрАдрес;
 
-  if (formFile == 'form_C_22_10e.pdf'){
+      var addr = selectedOrgExtData.СвязРуковод[0].ЮрАдрес.split(',', 2);
+      orgDataset[orgIndex].Индекс = addr[0].trim();
+      orgDataset[orgIndex].АдресБезИндекса = selectedOrgExtData.СвязРуковод[0].ЮрАдрес.replace(addr[0] + ',', '').trim();      
+      orgDataset[orgIndex].Индекс = selectedOrgExtData.СвязРуковод[0].ЮрАдрес;
+    }
+
+  
+  if (formFile == 'form_C_22_10e.pdf'){    
+    
     fillFormField(form,'Text10',orgDataset[orgIndex].НаимПолн, customFont);
     fillFormField(form,'Text21111',orgDataset[orgIndex].НаимПолн, customFont);
     fillFormField(form,'Text21139',orgDataset[orgIndex].НаимПолн, customFont);
@@ -303,6 +263,9 @@ console.log(form.getFields());
     fillFormField(form,'Text21139',orgDataset[orgIndex].ОГРНИП, customFont);
 
     fillFormField(form,'Text21112',orgDataset[orgIndex].ЮрАдрес, customFont);
+
+    fillFormField(form,'Text211',orgDataset[orgIndex].АдресБезИндекса, customFont);
+    fillFormField(form,'Text222',orgDataset[orgIndex].Индекс, customFont);    
   }
   
   if (formFile == 'form_F_22_10e.pdf'){
@@ -314,6 +277,8 @@ console.log(form.getFields());
     fillFormField(form,'Text6',orgDataset[orgIndex].ИНН, customFont);
     
     fillFormField(form,'Text15',orgDataset[orgIndex].ЮрАдрес, customFont); 
+    fillFormField(form,'Text16',orgDataset[orgIndex].Индекс, customFont); 
+    fillFormField(form,'Text222',orgDataset[orgIndex].Индекс, customFont);
 
     fillFormField(form,'Text115',orgDataset[orgIndex].ИНН, customFont);    
     fillFormField(form,'Text2070',orgDataset[orgIndex].ИНН, customFont);
@@ -334,6 +299,7 @@ console.log(form.getFields());
     fillFormField(form,'Text21139',orgDataset[orgIndex].ОГРНИП, customFont);
     fillFormField(form,'Text21144',orgDataset[orgIndex].ИНН, customFont);
   }
+})
   
   //nameField.updateAppearances(customFont)
   //, (field, widge, font) => {      console.log(font.encoding)    })
